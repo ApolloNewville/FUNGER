1f3169fc42fe5ddca7226a0a4d1008a32074fd79
Domek Cataliss <leonardoc1111@hotmail.com>
2026-01-06 23:24:11 -0600

    2026-01-06.4 Finally cleaning function done!

--- Transformation/cleaning.py
+++ Transformation/cleaning.py
@@ -1,11 +1,22 @@
 import pandas as pd
+import numpy as np
+import re
+from typing import Literal
 
-def normal_cleaning(df):
-    def df_cleaning(df):
-        pass
-    def column_cleaning():
-        pass
-    def cell_cleaning ():
-        pass
+# This is the re pattern to determine columns that only has integers.
+number_re = re.compile(r'^\d+(\.\d+)?$')  
 
-    return 
+# This is 
+
+def df_cleaning(
+        df: pd.DataFrame,
+        dropna_how: Literal['any','all'] = 'any') -> pd.DataFrame:
+    if dropna_how not in ('all','any'): 
+        raise ValueError('Write all or any to declare the process of dropna')    
+    df = df.replace(to_replace='',value=np.nan)
+    df = df.infer_objects(copy=False)
+    df = df.dropna(how=dropna_how,axis='index')
+    return df
+
+def cleaning_column(column: pd.Series):
+    column.str.strip().str.lower()
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 # Modules
 import pandas as pd
 from Load.data_loading import extraction
-
+from Transformation.cleaning import df_cleaning
 # Paths
 folder_path = r'Data\raw data'
 files_interest = ['Weapons','Skills','Armors','Classes','Enemies']
+
 
 dfs = extraction(path = folder_path,
                  keywords = files_interest)
 
 dfs = dfs.copy()
+
+for key, df in dfs.items():
+    dfs[key] = df_cleaning(df=df, dropna_how='all')
+
+print(dfs)
