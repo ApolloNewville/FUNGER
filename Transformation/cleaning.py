9db009848f2c75bb72f1c7b2fdb4728f3d31a62e
Domek Cataliss <leonardoc1111@hotmail.com>
2026-01-07 20:07:11 -0600

    2026-01-07.2

--- Transformation/cleaning.py
+++ Transformation/cleaning.py
@@ -11,12 +11,18 @@ number_re = re.compile(r'^\d+(\.\d+)?$')
 def df_cleaning(
         df: pd.DataFrame,
         dropna_how: Literal['any','all'] = 'any') -> pd.DataFrame:
+    # Validation of inputs
     if dropna_how not in ('all','any'): 
         raise ValueError('Write all or any to declare the process of dropna')    
-    df = df.replace(to_replace='',value=np.nan)
+    # Removing '' values from data frames
+    df = df.replace(to_replace='',value=np.nan).replace(to_replace=r'[\n\t]|\\n|\\t',value=' ',regex=True)
     df = df.infer_objects(copy=False)
     df = df.dropna(how=dropna_how,axis='index')
     return df
 
-def cleaning_column(column: pd.Series):
-    column.str.strip().str.lower()
+def str_cleaning(df: pd.DataFrame) -> pd.DataFrame:
+    if not isinstance(df,pd.DataFrame):
+        raise TypeError('Object must be a pd.DataFrame')
+    def clean_cell(x:str):
+        return x.strip().lower() if isinstance(x,str) else x
+    return df.copy().apply(lambda col: col.map(clean_cell))
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 # Modules
 import pandas as pd
 from Load.data_loading import extraction
-from Transformation.cleaning import df_cleaning
+from Transformation.cleaning import df_cleaning, str_cleaning
 # Paths
 folder_path = r'Data\raw data'
 files_interest = ['Weapons','Skills','Armors','Classes','Enemies']
 
-
+# Extraction of JSON data and convertion to data frames inside of the dictionary called dfs.
 dfs = extraction(path = folder_path,
                  keywords = files_interest)
 
+# Creation of a copy from dictionary dfs
 dfs = dfs.copy()
 
+
+# Iteration to clean data frames from dictionary dfs
 for key, df in dfs.items():
-    dfs[key] = df_cleaning(df=df, dropna_how='all')
+    df = df_cleaning(df=df, dropna_how='all')
+    df = str_cleaning(df=df)
+    dfs[key] = df
 
 print(dfs)
